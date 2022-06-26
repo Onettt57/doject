108fef0390087bfa011fdbbee24ab5cee850e4d7
vitom <thebestplayerintheworld100@gmail.com>
2022-06-26 11:44:03 +0100

    First commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Doject</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body class="antialiased h-screen bg-gradient-to-r from-purple-500 to-pink-500">
+
+<div class="inline-flex rounded-md shadow">
+    <a href="/" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-500 hover:bg-purple-500">Go Back</a>
+</div>
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Doject</title>
+
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="antialiased">
+
+
+
+    <div class="bg-zinc-800  flex items-center min-h-screen">
+        <div class="items-center justify-center max-w-7xl mx-auto py-12 px-4 sm:px-6">
+            <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                <span class="block text-sky-500">Doject</span>
+            </h2>
+            <div class="mt-8 flex">
+
+
+
+             <div class="inline-flex rounded-md shadow">
+                   <a href="/register" class="inline-flex px-5 py-3 border border-transparent text-base font-large rounded-md text-white bg-blue-600 hover:bg-indigo-700">Join Doject now</a>
+             </div>
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
