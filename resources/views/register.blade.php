c0c5bfac06754e27016d846bd1305d3bd7180bcb
vitom <thebestplayerintheworld100@gmail.com>
2022-07-04 12:06:55 +0100

    edited register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,19 +9,16 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
-<body class="antialiased h-screen bg-gradient-to-r from-purple-500 to-pink-500">
+<body class="antialiased">
+
 
 <div class="inline-flex rounded-md shadow">
-    <a href="/" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-500 hover:bg-purple-500">Go Back</a>
+<div class="inline-flex rounded-md shadow">
+    <a href="/" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-500 hover:bg-purple-500">Free 7 Day Trail</a>
+</div>
 </div>
 
 
-<div class="bg-gradient-to-r from-purple-500 to-pink-500  flex items-center min-h-screen">
-    <div class="items-center justify-center max-w-7xl mx-auto py-12 px-4 sm:px-6">
-<h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-    <span class="block h-60  text-sky-500">Subscription's</span>
-    <span class="block text-sky-500">Subscription</span>
-</h2>
 
 
 
